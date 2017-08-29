10c0a41bd6cdd789e9c33f5acc5d61f0d7e8b3f5
gegorov <gegorov@yahoo.com>
2017-08-29 12:56:35 +0300

    hexlet-JS_Intro_to_programming old tasks added

---
+++ intro_to_prog/m05/squareOfSum.js
@@ -0,0 +1,14 @@
+/*
+squareOfSum.js
+Реализуйте функцию squareOfSum, которая находит квадрат суммы двух чисел по формуле: a^2 + 2ab + b^2 (a^2 означает "a в степени 2" или "а в квадрате").
+
+squareOfSum(2, 3) // 25
+squareOfSum(1, 10) // 121
+Подсказка: в JavaScript нет оператора ^, поэтому вам нужно будет придумать как возвести число в квадрат.
+*/
+
+// BEGIN (write your solution here)
+const squareOfSum = (a, b) => (a * a) + (2 * a * b) + (b * b);
+// END
+
+export default squareOfSum;
