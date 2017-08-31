758709384a83a9b2aae3fae8f621d2af75aa4156
gegorov <gegorov@yahoo.com>
2017-09-01 00:18:31 +0300

    hexlet-scip some new tasks added

--- sicp/sicp_c1.lisp
+++ sicp/sicp_c1.lisp
@@ -34,3 +34,56 @@
   (if (< x 0) (- x) x))
 (define (square x)
   (* x x))
+
+;1.2.1 линейные рекурсия и итерация. вычисление факториала
+
+(define (factorial n)
+  (if (= n 1)
+    1
+    (* n (factorial(- n 1)))))
+
+
+(define (iFact n)
+  (define (iter accum counter)
+    (if (< n counter)
+      accum
+      (iter (* accum counter)
+            (+ counter 1))))
+  (iter 1 1))
+
+; Древовидная рекурсия,  последовательность чисел Фибоначчи
+
+(define (fib n)
+  (cond
+    ((= n 0) 0)
+    ((= n 1) 1)
+    (else (+ (fib (- n 1))
+             (fib (- n 2))))))
+
+(define (fib2 n)
+  (define (fib-iter a b counter)
+    (if (= counter 0)
+        b
+        (fib-iter (+ a b) a (- counter 1))))
+  (fib-iter 1 0 n))
+
+; Размен монет
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond
+    ((= amount 0) 1)
+    ((or (< amount 0) (= kinds-of-coins 0)) 0)
+    (else (+ (cc amount (- kinds-of-coins 1))
+             (cc (- amount (first-denomination kinds-of-coins))
+                 kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond
+    ((= kinds-of-coins 1) 1)
+    ((= kinds-of-coins 2) 5)
+    ((= kinds-of-coins 3) 10)
+    ((= kinds-of-coins 4) 25)
+    ((= kinds-of-coins 5) 50)))
--- sicp/sicp_c1_t_1_3.lisp
+++ sicp/sicp_c1_t_1_3.lisp
@@ -6,5 +6,5 @@
   (cond
     ((and (> a c) (> b c)) (sum-of-squares a b))
     ((and (> a b) (> c b)) (sum-of-squares a c))
-    (else (sum-of-squares b c))
-    ))
+    (else (sum-of-squares b c))))
+    
