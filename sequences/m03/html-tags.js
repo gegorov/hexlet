83c253fb7d0f7c87bfc6dd250cc704622096c725
gegorov <gegorov@yahoo.com>
2017-09-06 15:33:55 +0300

    hexlet-JSBackend-sequences added task defenition to m03

--- sequences/m03/html-tags.js
+++ sequences/m03/html-tags.js
@@ -1,3 +1,32 @@
+// html-tags.js
+// Реализуйте базовый интерфейс для создания html. Он включает в себя
+// следующие функции:
+//
+// make - конструктор
+// node - создает новый тег. Содержит два элемента, имя тега и его содержимое.
+// Дополнительно реализуйте селекторы тега: name и value
+// append - добавляет элемент (тег) в html-список
+// toString - возвращает текстовое представление html
+// Пример использования этого интерфейса:
+//
+// import { make, append, toString, node } from './html-tags';
+//
+// const dom1 = make();
+//
+// const dom2 = append(dom1, node('h1', 'hello, world'));
+// const dom3 = append(dom2, node('h2', 'header2'));
+//
+// const tag = node('h3', 'header3');
+// const dom = append(dom3, tag);
+//
+// toString(dom);
+// // <h1>hello, world</h1><h2>header2</h2><h3>header3</h3>
+//
+// // Пример без создания промежуточных переменных
+// toString(append(make(), node('p', 'this is Sparta!')));
+// // <p>this is Sparta!</p>
+// Экспортируйте все созданные функции.
+
 // eslint-disable-next-line
 import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs';
 // eslint-disable-next-line
