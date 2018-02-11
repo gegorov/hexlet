0c845c3146cee655ece16ae8034ecaf3feeb8b27
gegorov <gegorov@yahoo.com>
2018-02-11 14:05:24 +0300

    refactored reverse using recursion

--- intro_to_prog/m11/solution.js
+++ intro_to_prog/m11/solution.js
@@ -9,13 +9,26 @@ reverse('hello, world!'); // !dlrow ,olleh
 индекс последнего символа не совпадает с длиной строки.
 */
 // BEGIN (write your solution here)
-const reverse = (str) => {
-  let result = '';
-  for (let i = str.length - 1; i >= 0; i -= 1) {
-    result += str[i];
-  }
-  return result;
-};
+// const reverse = (str) => {
+//   let result = '';
+//   for (let i = str.length - 1; i >= 0; i -= 1) {
+//     result += str[i];
+//   }
+//   return result;
+// };
 // END
 
-export default reverse;
+const recReverse = (str) => {
+  const strLength = str.length;
+  if (strLength === 0) {
+    return str;
+  }
+
+  return `${str[strLength - 1]}${recReverse(str.slice(0, strLength - 1))}`;
+};
+
+const myStr = 'Lorem ipsum';
+console.log(recReverse(myStr));
+
+
+export default recReverse;
