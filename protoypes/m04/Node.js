690e80dddb05ae7da27e0bd17e1095d39ed7e711
gegorov <gegorov@yahoo.com>
2019-02-05 23:12:53 +0300

    feat: add prototypes m04 solution

---
+++ protoypes/m04/Node.js
@@ -0,0 +1,13 @@
+// BEGIN (write your solution here)
+export default class {
+  constructor(name, attributes) {
+    this.name = name;
+    this.attributes = attributes;
+  }
+
+  getAttributesAsLine() {
+    return Object.keys(this.attributes)
+      .reduce((acc, key) => `${acc} ${key}="${this.attributes[key]}"`, '');
+  }
+}
+// END
