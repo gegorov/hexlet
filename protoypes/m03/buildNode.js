ab19e9d39fd066f682b3d6308fbe650ddbafec30
gegorov <gegorov@yahoo.com>
2019-02-05 16:11:40 +0300

    feat: add prototypes solution to m03

---
+++ protoypes/m03/buildNode.js
@@ -0,0 +1,10 @@
+import PairedTag from './PairedTag';
+import SingleTag from './SingleTag';
+
+// BEGIN (write your solution here)
+const singleTagsList = new Set(['hr', 'br', 'img']);
+export default (name, ...args) => {
+  const C = singleTagsList.has(name) ? SingleTag : PairedTag;
+  return new C(name, ...args);
+};
+// END
